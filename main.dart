69b1fdd39ed983a020b62d70b7ce6f146398bf03
Byeol2codingtest <69844005+Byeol2codingtest@users.noreply.github.com>
2023-04-03 13:03:06 +0900

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,35 @@
+import 'package:chatting_app/screens/chat_screen.dart';
+import 'package:chatting_app/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //firebase 사용 시 필요함.
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ChatScreen();
+          }
+          return LoginSignupScreen();
+        },
+      ),
+    );
+  }
+}
